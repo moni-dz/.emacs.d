22ffc69e311a342f5952057177eca33c5c3d8b87
fortuneteller2k <lythe1107@gmail.com>
2021-04-29 11:38:08 +0800

    move frame modifications off interface.el, use helpful

--- elisp/interface.el
+++ elisp/interface.el
@@ -1,30 +1,20 @@
-(tooltip-mode -1)
-
-(push '(internal-border-width . 15) default-frame-alist)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
-
-(setq ring-bell-function 'ignore
-      visible-bell nil
-      uniquify-buffer-name-style 'post-forward-angle-brackets
-      inhibit-startup-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message t
-      initial-scratch-message nil
-      enable-recursive-minibuffers t)
-
 (use-package selectrum
-  :init
+  :config
   (selectrum-mode +1))
 
-(use-package doom-themes
-  :init
-  (load-theme 'doom-horizon t)
+(use-package helpful
+  :bind
+  (("C-h f" . helpful-callable)
+   ("C-h v" . helpful-variable)
+   ("C-h k" . helpful-key))
   :config
+  (custom-theme-set-faces
+   'user
+   '(helpful-heading ((t (:inherit variable-pitch))))))
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-horizon t)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config)
   :custom
@@ -42,16 +32,24 @@
 
 (use-package doom-modeline
   :hook
-  (after-init . doom-modeline-mode)
+  (emacs-startup . doom-modeline-mode)
   :custom
   (doom-modeline-height 20)
   (doom-modeline-bar-width 4)
   (doom-modeline-icon nil)
+  (doom-modeline-enable-word-count t)
   (doom-modeline-indent-info t))
 
 (use-package olivetti
   :custom
   (olivetti-body-width 120))
+
+(use-package display-line-numbers
+  :hook
+  ((prog-mode org-mode) . display-line-numbers-mode)
+  :custom
+  (display-line-numbers-width 3)
+  (display-line-numbers-widen t))
 
 (defun interface/toggle-zen-mode ()
   "Toggle a distraction-free environment for writing."
@@ -65,12 +63,7 @@
          (centaur-tabs-local-mode +1)
          (display-line-numbers-mode -1))))
 
-(use-package display-line-numbers
-  :hook
-  ((prog-mode org-mode) . display-line-numbers-mode)
-  :custom
-  (display-line-numbers-width 3)
-  (display-line-numbers-widen t))
+(global-set-key (kbd "C-x z") 'interface/toggle-zen-mode)
 
 (use-package which-key
   :hook
@@ -79,6 +72,7 @@
 (use-package page-break-lines)
 
 (use-package dashboard
+  :requires page-break-lines
   :preface
   (defun dashboard-init-info-with-gcs ()
     "Set a dashboard banner including information on package initialization
@@ -89,11 +83,13 @@
   :config
   (dashboard-setup-startup-hook)
   :custom
-  (dashboard-startup-banner (concat user-emacs-directory "assets/emacs.png"))
+  (dashboard-startup-banner
+   (concat user-emacs-directory "assets/emacs.png"))
   (dashboard-banner-logo-title "everyone's favorite lisp interpreter")
   (dashboard-center-content t)
   (dashboard-footer-icon "")
-  (dashboard-footer-messages '("When I say a thing, you know it's true. So I'm calling it right here and now. This one's in the bag!"))
+  (dashboard-footer-messages
+   '("When I say a thing, you know it's true. So I'm calling it right here and now. This one's in the bag!"))
   (dashboard-items '((recents . 10)))
   (dashboard-show-shortcuts nil)
   :hook
